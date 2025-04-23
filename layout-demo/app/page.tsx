d0a87e6a9d2dbc993910a5a5fb3ee3fa112e378b
rachna <rachnatweeny@gmail.com>
2025-04-23 12:18:09 +0530

    added images using remote image

--- layout-demo/app/page.tsx
+++ layout-demo/app/page.tsx
@@ -6,6 +6,12 @@ export default function Home() {
   return (
     <div>
       <Link href="/blog">Blog</Link> 
+      <Image
+      src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      alt="Picture of the author"
+      width={50}
+      height={50}
+    />
       
       
       
--- layout-demo/next.config.ts
+++ layout-demo/next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here */images: {
+    domains: ['images.pexels.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: 'photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"**',
+        search: '',
+      },
+    ],
+  },
+  experimental: {
+    authInterrupts: true,
+  },
 };
 
 export default nextConfig;
